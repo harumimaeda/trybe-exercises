3432901e3e23d5213d88f0ec8ef917b3a2365bc5
Harumi Maeda <harumiumaeda@gmail.com>
2022-03-07 15:50:46 -0300

    Exercicios dia 4.1

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/script.js
@@ -0,0 +1,26 @@
+const myName = 'Isabela';
+const birthCity = 'São Paulo';
+let birthYear = 1997;
+
+// console.log(myName);
+// console.log(birthCity);
+// console.log(birthYear);
+
+// birthYear = 2030;
+
+// console.log(birthYear);
+
+// const birthCity = 'Fortaleza';
+
+// console.log(birthCity);
+// Ocorre esse erro pois o birthCity é uma CONSTANTE, não podendo ter seu valor reatribuído.
+
+const base = 5;
+let height = 8;
+const area = base * height;
+
+console.log(area);
+
+const perimeter = base * 2 + height * 2;
+
+console.log(perimeter);

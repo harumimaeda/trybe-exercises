d36b3e6d9cb2418278eda5ba50c6c19d8ab3d362
Harumi Maeda <harumiumaeda@gmail.com>
2022-03-07 18:12:36 -0300

    Adiciona diretorios do dia 4.1

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/script.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/script.js
@@ -15,12 +15,22 @@ let birthYear = 1997;
 // console.log(birthCity);
 // Ocorre esse erro pois o birthCity é uma CONSTANTE, não podendo ter seu valor reatribuído.
 
-const base = 5;
-let height = 8;
-const area = base * height;
+// const base = 5;
+// let height = 8;
+// const area = base * height;
 
-console.log(area);
+// console.log(area);
 
-const perimeter = base * 2 + height * 2;
+// const perimeter = base * 2 + height * 2;
 
-console.log(perimeter);
+// console.log(perimeter);
+
+const notaDesafio = 78;
+
+if (notaDesafio >= 80){
+    console.log("Parabéns, você foi aprovada(o)!")
+}else if (notaDesafio < 80 && notaDesafio >= 60){
+    console.log("Você está na nossa lista de espera")
+}else if(notaDesafio < 60){
+    console.log("Você foi reprovada(o)")
+}

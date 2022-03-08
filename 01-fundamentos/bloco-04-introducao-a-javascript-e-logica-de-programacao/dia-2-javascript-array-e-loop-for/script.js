0897aec732b8a2e97679f4a973751a3c82dc2a7b
Harumi Maeda <harumiumaeda@gmail.com>
2022-03-08 18:26:13 -0300

    Organiza os arquivos

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,37 +1,3 @@
-// 1. Obtenha o valor "Serviços" do array menu :
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let menuServices = menu[1];
-
-console.log(menuServices);
-
-// 2. Procure o índice do valor "Portfólio" do array menu :
-
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let indexOfPortfolio = menu.index[2];
-
-console.log(indexOfPortfolio);
-
-// 3. Adicione o valor "Contato" no final do array menu :
-
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-menu.push('Contato')
-
-console.log(menu);
-
-// 1. Utilize o for para imprimir os elementos da lista groceryList com o console.log() :
-
-let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
-
-for(var index = 0; index < groceryList.length; index +=1){
-    console.log(groceryList[index]);
-}
-
-
-// 2. Utilize o for/of para imprimir os elementos da lista names com o console.log() :
-
-let names = ['João', 'Maria', 'Antônio', 'Margarida'];
-
-for(let name of names){
-    console.log(name);
-}
+// 1. Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;

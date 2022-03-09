e7b9d21defb18e4fbbcf32d296fee0f325f82a8f
Harumi Maeda <harumiumaeda@gmail.com>
2022-03-08 23:00:09 -0300

    Tentando resolver exercicio 5 e 6

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -22,10 +22,52 @@ console.log('Exercício 3: ' + average);
 
 // 4. Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
 
-if(average > 20){
+if(average > 20) {
     console.log("Valor maior que 20.")
 }else if(average < 20){
     console.log("Valor menor que 20.")
 }else{
     console.log("Erro!")
+}
+
+// 5. Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+// !!!!!!
+
+let higherNumber = numbers[0];
+
+for (let i = 1; i < numbers.length; i += 1) {
+  if (numbers[i] > higherNumber) {
+    higherNumber = numbers[i];
+  }
+}
+
+console.log(higherNumber);
+
+// 6. Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let isOdd = numbers[i];
+
+for(let i = 0; i < numbers.length; i += 1) {
+    if(numbers[i] % 0){
+        console.log("Nenhum valor ímpar encontrado.")
+    }else if(numbers[i] %! 0){
+        console.log(isOdd[ ])
+    }
+}
+
+----------------------
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let result = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] % 2 !== 0) {
+    result += 1;
+  }
+}if (result === 0) {
+  console.log('nenhum valor ímpar encontrado');
+} else {
+  console.log(result);
 }

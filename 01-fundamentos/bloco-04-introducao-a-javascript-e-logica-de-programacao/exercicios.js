0ab8aaa43b0e7a3c2f96513e700842f9348446c5
Harumi Maeda <harumiumaeda@gmail.com>
2022-03-10 18:34:37 -0300

    Realiza ex 6 de exercicios.js

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/exercicios.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/exercicios.js
@@ -63,7 +63,7 @@ let info2 = {
     personagem: 'Tio Patinhas',
     origem: ['Christmas on Bear Mountain', 'Dells Four Color Comics #178'],
     nota: 'O último MacPatinhas',
-    recorrente: 'Sim'
+    recorrente: 'Sim'x
 }
 
 for (properties in info){
@@ -77,3 +77,20 @@ for (properties in info){
             console.log(info[properties] + ' & ' + info2[properties]);
         }
     }
+
+    // 6 - Acesse as chaves nome , sobrenome e titulo , que está dentro da chave livrosFavoritos , e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+        {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+        },
+    ],
+    };
+
+    console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama '" + leitor['livrosFavoritos'][0]['titulo'] + "'.")

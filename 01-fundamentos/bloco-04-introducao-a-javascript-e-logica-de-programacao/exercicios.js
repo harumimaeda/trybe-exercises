408f0333733643c0378c449cc1f71a6ac11aa124
Harumi Maeda <harumiumaeda@gmail.com>
2022-03-10 18:27:39 -0300

    Realiza ex 5 de exercicios.js

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/exercicios.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/exercicios.js
@@ -33,5 +33,47 @@ console.log(info);
 // recorrente
 
 for(key in info){
-    console.log(key)
+    console.log(key);
 }
+
+// 4 - Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
+// Margarida
+// Pato Donald
+// Namorada do personagem principal nos quadrinhos do Pato Donald
+// Sim
+
+for(i in info){
+    console.log(info[i]);
+}
+
+// 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. Valor esperado no console:
+// // Margarida e Tio Patinhas
+// Pato Donald e Christmas on Bear Mountain, Dell's Four Color Comics #178
+// Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas
+// Ambos recorrentes // Atenção para essa última linha!
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim'
+  };
+
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: ['Christmas on Bear Mountain', 'Dells Four Color Comics #178'],
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim'
+}
+
+for (properties in info){
+    if(
+        properties === 'recorrente' &&
+        info[properties] === 'Sim' &&
+        info2[properties] === 'Sim'
+    ){
+        console.log('Ambos recorrentes');
+    }else{
+            console.log(info[properties] + ' & ' + info2[properties]);
+        }
+    }

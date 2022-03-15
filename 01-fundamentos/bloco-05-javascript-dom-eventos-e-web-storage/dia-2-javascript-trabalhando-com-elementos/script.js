628bb659a7549289244082432fbc92e62c8e2c1c
Harumi Maeda <harumiumaeda@gmail.com>
2022-03-15 15:52:25 -0300

    Resolve parte dos problemas propostos na segunda parte dos exercicios

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -21,3 +21,27 @@ console.log(document.getElementById("elementoOndeVoceEsta").nextElementSibling);
 
 // Agora acesse o terceiroFilho a partir de pai .
 console.log(document.getElementById("pai").children[2]);
+
+// Crie um irmão para elementoOndeVoceEsta.
+let maisUmFilho = document.createElement("div");
+maisUmFilho.innerText = "maisUmFilho";
+
+elementoOndeVoceEsta.parentElement.appendChild(maisUmFilho);
+
+// Crie um filho para elementoOndeVoceEsta .
+let oFilhoDoElemento = document.createElement("div");
+
+elementoOndeVoceEsta.appendChild(oFilhoDoElemento);
+
+oFilhoDoElemento.innerText = "oFilhoDoElemento";
+
+// Crie um filho para primeiroFilhoDoFilho .
+let primeiroDoPrimeiro = document.createElement("div");
+
+primeiroFilhoDoFilho.appendChild(primeiroDoPrimeiro);
+
+primeiroDoPrimeiro.innerHTML = "primeiroDoPrimeiro";
+
+// A partir desse filho criado, acesse terceiroFilho.
+
+console.log(document.getElementById("primeiroDoPrimeiro").parentElement.nextElementSibling);

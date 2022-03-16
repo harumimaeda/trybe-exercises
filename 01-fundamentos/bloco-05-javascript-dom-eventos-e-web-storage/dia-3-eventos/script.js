89ba1b4461589ef01f20adf20b3a2dab2871027b
Harumi Maeda <harumiumaeda@gmail.com>
2022-03-16 15:44:21 -0300

    Resolve exercicio 1.2

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-eventos/script.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-eventos/script.js
@@ -7,7 +7,8 @@ const myWebpage = document.getElementById('my-spotrybefy');
 
 // 1. Copie esse arquivo e edite apenas ele;
 // 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
-// 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
+/* 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
+No CSS, existe uma classe (tech) na linha 68 que recebe uma declaração que reposiciona a classe verticalmente. */
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?

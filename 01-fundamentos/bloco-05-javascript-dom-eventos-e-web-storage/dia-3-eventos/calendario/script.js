4b0a9fe3b7e06d298a440ae5e63f6610a2d554a0
Harumi Maeda <harumiumaeda@gmail.com>
2022-03-17 23:43:16 -0300

    Resolve exercicio 2

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-eventos/calendario/script.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-eventos/calendario/script.js
@@ -13,7 +13,7 @@ function createDaysOfTheWeek() {
 
 createDaysOfTheWeek();
 
-// O array dezDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Desenvolva uma função que crie dinamicamente cada dia do calendário e os adicione como filhos/filhas da tag <ul> com ID "days" . Note que os dias 29 e 30 de novembro estão no array pois representam respectivamente Domingo e Segunda-feira.
+// 1. O array dezDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Desenvolva uma função que crie dinamicamente cada dia do calendário e os adicione como filhos/filhas da tag <ul> com ID "days" . Note que os dias 29 e 30 de novembro estão no array pois representam respectivamente Domingo e Segunda-feira.
 // Os dias devem estar contidos em uma tag <li> , e todos devem ter a classe day . Ex: <li class="day">3</li>
 // Os dias 24, 25 e 31 são feriados e, além da classe day , devem conter também a classe holiday . Ex: <li class="day holiday">24</li>
 // Os dias 4, 11, 18 e 25 são Sexta-feira. Eles devem conter a classe day e a classe friday . Ex: <li class="day friday">4</li>
@@ -41,6 +41,16 @@ function createDaysOfMonth() {
 
 createDaysOfMonth();
 
-const beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
+// 2. Implemente uma função que receba como parâmetro a string "Feriados" e crie dinamicamente um botão com o nome "Feriados".
+// Adicione a este botão a ID "btn-holiday" .
+// Adicione este botão como filho/filha da tag <div> com classe "buttons-container" .
 
-console.log(beasts.indexOf('bison'));
+function feriados(Feriado) {
+  const newButton = document.createElement('button');
+
+  document.getElementsByClassName('buttons-container')[0].appendChild(newButton);
+  document.getElementsByTagName('button')[0].innerText = Feriado;
+  document.getElementsByTagName('button')[0].id = 'btn-holiday';
+}
+
+feriados("Feriado");

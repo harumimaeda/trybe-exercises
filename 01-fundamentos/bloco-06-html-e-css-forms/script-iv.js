a57f6c81dfab5284c13a2fcd22dcb28d9548e4b6
Harumi Maeda <harumiumaeda@gmail.com>
2022-03-24 18:47:02 -0300

    Exercicio 3

--- 01-fundamentos/bloco-06-html-e-css-forms/script-iv.js
+++ 01-fundamentos/bloco-06-html-e-css-forms/script-iv.js
@@ -4,5 +4,13 @@ const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
 const HREF_LINK = document.querySelector("#href");
 
 HREF_LINK.addEventListener("click", function(event){
-    event.preventDefault()
+  event.preventDefault();
 });
+
+INPUT_CHECKBOX.addEventListener('click', function(event){
+  event.preventDefault();
+})
+
+INPUT_TEXT.addEventListener('beforeinput', function(event){
+  event.preventDefault();
+})

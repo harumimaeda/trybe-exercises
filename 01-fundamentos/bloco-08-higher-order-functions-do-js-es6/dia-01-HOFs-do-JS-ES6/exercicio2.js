e99bf93e4e999517d6a29a4883d79d3387aa3a6b
Harumi Maeda <harumiumaeda@gmail.com>
2022-04-08 15:47:39 -0300

    Cria e arquivos do dia 8.2

--- 01-fundamentos/bloco-08-higher-order-functions-do-js-es6/dia-01-HOFs-do-JS-ES6/exercicio2.js
+++ 01-fundamentos/bloco-08-higher-order-functions-do-js-es6/dia-01-HOFs-do-JS-ES6/exercicio2.js
@@ -29,3 +29,14 @@ Seu chute: ${chute}`);
 }
 
 console.log(resultadoSorteio(4, numberChecker));
+
+// 3 - Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
+
+const funcao = (rightAnswers, studentAnswers, callback) => {
+  for (let i = 0; i <= rightAnswers.length; i += 1) {
+
+  }
+}
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
